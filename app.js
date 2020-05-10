b7e09c17bc42d119e00cff4ab212c46f09232449
bbanderson <tombyun@naver.com>
2020-05-10 18:05:13 +0900

    Add logic on app.js

--- app.js
+++ app.js
@@ -5,4 +5,18 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("view engine", "ejs");
 
+app.get("/", function(req, res) {
+
+    const today = new Date()
+    const currentDay = today.getDay();
+    let day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        // WEEKEND!
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+})
+
 app.listen(3000, ()=>console.log("Server has connected!"))
