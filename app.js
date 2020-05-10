3e4d9f88cc4a61a9a1570e24feecb07c8cf08673
bbanderson <tombyun@naver.com>
2020-05-10 18:28:03 +0900

    Add array of days

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ app.get("/", function(req, res) {
 
     const today = new Date()
     const currentDay = today.getDay();
-    let day = "";
+    let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    if (currentDay === 6 || currentDay === 0) {
-        // WEEKEND!
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
-    res.render("list", {kindOfDay: day})
+    // if (currentDay === 6 || currentDay === 0) {
+    //     // WEEKEND!
+    //     day = "Weekend";
+    // } else {
+    //     day = "Weekday";
+    // }
+    res.render("list", {kindOfDay: day[currentDay], day: day})
 })
 
 app.listen(3000, ()=>console.log("Server has connected!"))
